6d2ab7ca4cf5c5d0689a37342568594572e8b3e1
Maryam <mm10arya@gmail.com>
2024-07-08 13:36:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+<a href="https://github.com/MaryNfs/MaryNfs">
+  <picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/MaryNfs/MaryNfs/main/dark_mode.svg">
+    <img alt="Maryam Nafisi's GitHub Profile README" src="https://raw.githubusercontent.com/MaryNfs/MaryNfs/main/light_mode.svg">
+  </picture>
+</a>
---
+++ today.py
@@ -0,0 +1,136 @@
+import datetime
+from dateutil import relativedelta
+import requests
+import os
+from xml.dom import minidom
+import time
+import hashlib
+
+# Personal access token with permissions: read:enterprise, read:org, read:repo_hook, read:user, repo
+HEADERS = {'authorization': 'token '+ os.environ['ACCESS_TOKEN']}
+USER_NAME = os.environ['USER_NAME'] # 'Andrew6rant'
+QUERY_COUNT = {'user_getter': 0}
+
+
+def daily_readme(birthday):
+    """
+    Returns the length of time since I was born
+    e.g. 'XX years, XX months, XX days'
+    """
+    diff = relativedelta.relativedelta(datetime.datetime.today(), birthday)
+    return '{} {}, {} {}, {} {}{}'.format(
+        diff.years, 'year' + format_plural(diff.years), 
+        diff.months, 'month' + format_plural(diff.months), 
+        diff.days, 'day' + format_plural(diff.days),
+        ' 🎂' if (diff.months == 0 and diff.days == 0) else '')
+
+
+def format_plural(unit):
+    """
+    Returns a properly formatted number
+    e.g.
+    'day' + format_plural(diff.days) == 5
+    >>> '5 days'
+    'day' + format_plural(diff.days) == 1
+    >>> '1 day'
+    """
+    return 's' if unit != 1 else ''
+
+
+def simple_request(func_name, query, variables):
+    """
+    Returns a request, or raises an Exception if the response does not succeed.
+    """
+    request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables':variables}, headers=HEADERS)
+    if request.status_code == 200:
+        return request
+    raise Exception(func_name, ' has failed with a', request.status_code, request.text, QUERY_COUNT)
+
+def svg_overwrite(filename, age_data):
+    """
+    Parse SVG files and update elements with my age, commits, stars, repositories, and lines written
+    """
+    svg = minidom.parse(filename)
+    f = open(filename, mode='w', encoding='utf-8')
+    tspan = svg.getElementsByTagName('tspan')
+    # tspan[30].firstChild.data = age_data
+    uptime_element = svg.getElementById('uptime-value')
+    uptime_element.textContent = age_data
+
+
+    f.write(svg.toxml('utf-8').decode('utf-8'))
+    f.close()
+
+
+def svg_element_getter(filename):
+    """
+    Prints the element index of every element in the SVG file
+    """
+    svg = minidom.parse(filename)
+    open(filename, mode='r', encoding='utf-8')
+    tspan = svg.getElementsByTagName('tspan')
+    for index in range(len(tspan)): print(index, tspan[index].firstChild.data)
+
+
+def user_getter(username):
+    """
+    Returns the account ID and creation time of the user
+    """
+    query_count('user_getter')
+    query = '''
+    query($login: String!){
+        user(login: $login) {
+            id
+            createdAt
+        }
+    }'''
+    variables = {'login': username}
+    request = simple_request(user_getter.__name__, query, variables)
+    return {'id': request.json()['data']['user']['id']}, request.json()['data']['user']['createdAt']
+
+def query_count(funct_id):
+    """
+    Counts how many times the GitHub GraphQL API is called
+    """
+    global QUERY_COUNT
+    QUERY_COUNT[funct_id] += 1
+
+
+def perf_counter(funct, *args):
+    """
+    Calculates the time it takes for a function to run
+    Returns the function result and the time differential
+    """
+    start = time.perf_counter()
+    funct_return = funct(*args)
+    return funct_return, time.perf_counter() - start
+
+
+def formatter(query_type, difference, funct_return=False, whitespace=0):
+    """
+    Prints a formatted time differential
+    Returns formatted result if whitespace is specified, otherwise returns raw result
+    """
+    print('{:<23}'.format('   ' + query_type + ':'), sep='', end='')
+    print('{:>12}'.format('%.4f' % difference + ' s ')) if difference > 1 else print('{:>12}'.format('%.4f' % (difference * 1000) + ' ms'))
+    if whitespace:
+        return f"{'{:,}'.format(funct_return): <{whitespace}}"
+    return funct_return
+
+
+if __name__ == '__main__':
+    """
+    """
+    print('Calculation times:')
+    # define global variable for owner ID and calculate user's creation date
+    # e.g {'id': 'MDQ6VXNlcjU3MzMxMTM0'} and 2019-11-03T21:15:07Z for username 'Andrew6rant'
+    user_data, user_time = perf_counter(user_getter, USER_NAME)
+    OWNER_ID, acc_date = user_data
+    formatter('account data', user_time)
+    age_data, age_time = perf_counter(daily_readme, datetime.datetime(1994, 2, 20))
+    formatter('age calculation', age_time)
+    
+    svg_overwrite('dark_mode.svg', age_data)
+    svg_overwrite('light_mode.svg', age_data)
+
+    
